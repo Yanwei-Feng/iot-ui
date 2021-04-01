f55fed7cc1e9d79931e077f92e517409c2bfec71
冯砚伟 <fengyanwei@jd.com>
2021-04-01 17:58:09 +0800

    1.添加gitignore 忽略文件

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,11 @@ const paths = {
     dist: 'dist', // umd文件存放的目录名 - 暂时不关心
   },
   styles: 'components/**/*.less', // 样式文件路径 - 暂时不关心
-  scripts: ['components/**/*.{ts,tsx}',
-            '!components/**/demo/*.{ts,tsx}',
-            '!components/**/__tests__/*.{ts,tsx}'], // 脚本文件路径
+  scripts: [
+    'components/**/*.{ts,tsx}',
+    '!components/**/demo/*.{ts,tsx}',
+    '!components/**/__tests__/*.{ts,tsx}',
+  ], // 脚本文件路径
 };
 
 /**
